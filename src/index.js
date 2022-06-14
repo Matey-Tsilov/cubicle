1bbea512a0621074ae1648a043259e3e0c610c57
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2022-06-15 00:31:47 +0300

    Add layout's main.hbs

--- src/index.js
+++ src/index.js
@@ -14,5 +14,14 @@ app.set('views', './src/views')
 app.get('/', (req, res) => {
     res.render('index')
 })
+app.get('/create', (req, res) => {
+    res.render('create')
+})
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+app.all('',(req, res) => {
+    res.render('404')
+})
 
 app.listen(port, console.log(`App is listening on port ${port} ...`));
