e864472195896bdf5ba9d303fcd7956ecc9e2f79
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2022-06-14 22:40:25 +0300

    Render homepage
    - Move public to root folder
    - Rewrite all hrefs on other templates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const port = 4000
 const app = express()
+const handlebars = require('express-handlebars')
+
+app.use('/static', express.static('public'))
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', './src/views')
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the workshop!')
+    res.render('index')
 })
 
 app.listen(port, console.log(`App is listening on port ${port} ...`));
